17f85251b1699da8caaef215b267dc51ea0be776
WaniFi <skelpd24@gmail.com>
2018-08-09 19:13:05 +0900

    2747 문제 풀이 완료

---
+++ baekjoon_2747/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n1 = 0, n2 = 1, tmp, i1;
+	cin >> i1;
+	if (i1 < 2) cout << i1;
+	else {
+		for (int i = 2; i <= i1; i++) {
+			tmp = n1 + n2;
+			n1 = n2;
+			n2 = tmp;
+		}
+		cout << n2;
+	}
+	return 0;
+}
